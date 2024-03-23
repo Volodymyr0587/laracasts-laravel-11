8498d17077fcf34292107e0f3a5db1682b4a9db3
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-23 15:44:38 +0200

    display job details

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,9 +3,9 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
 </x-layout>
